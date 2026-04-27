f1eb3c3d9403c4973f3616d84eb12c5dd1b39f58
wingroom-ip <blinauthor@gmail.com>
2026-04-27 18:42:44 +0300

    second realisation

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -9,5 +9,5 @@ type Personal struct {
 }
 
 func (p Personal) Print() {
-	fmt.Printf("Имя: %s\nВес: %.2f\nРост: %.2f\n", p.Name, p.Weight, p.Height)
+	fmt.Printf("Имя: %s\nВес: %.2f кг.\nРост: %.2f м.\n", p.Name, p.Weight, p.Height)
 }
